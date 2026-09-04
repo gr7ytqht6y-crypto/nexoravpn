b68fe27cb55597bab0c772414b591d98f2162b4e
gr7ytqht6y-crypto <gr7ytqht6y@privaterelay.appleid.com>
2026-09-04 23:39:23 +0300

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,4 +1,24 @@
-export default {
+async function hashToken(token) {
+  const data = new TextEncoder().encode(token);
+
+  const hashBuffer = await crypto.subtle.digest(
+    "SHA-256",
+    data
+  );
+
+  return Array.from(new Uint8Array(hashBuffer))
+    .map((b) => b.toString(16).padStart(2, "0"))
+    .join("");
+}
+
+function generateToken() {
+  const bytes = new Uint8Array(32);
+  crypto.getRandomValues(bytes);
+
+  return Array.from(bytes)
+    .map((b) => b.toString(16).padStart(2, "0"))
+    .join("");
+}export default {
   async fetch(request, env) {
     const url = new URL(request.url);
     // =========================
